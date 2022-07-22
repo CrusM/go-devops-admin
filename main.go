5c64002ae9674329b1d0d9583fdd226b3d802871
蒋宏飞 <jianghongfei@cnstrong.cn>
2022-07-22 16:49:23 +0800

    air自动重载

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
  * @Author: 蒋宏飞 jianghongfei@cnstrong.cn
  * @Date: 2022-07-22 16:34:15
  * @LastEditors: 蒋宏飞 jianghongfei@cnstrong.cn
- * @LastEditTime: 2022-07-22 16:40:15
+ * @LastEditTime: 2022-07-22 16:44:26
  * @FilePath: \go-devops-admin\main.go
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,5 +13,5 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello word")
+	fmt.Println("Hello word2")
 }
