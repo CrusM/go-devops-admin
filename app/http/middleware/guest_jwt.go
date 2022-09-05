cc27373c138d59e9052603f09230deee731ce6ed
蒋宏飞 <jianghongfei@cnstrong.cn>
2022-09-05 17:26:20 +0800

    Guest 中间件, 后续游客访问接口使用

---
+++ app/http/middleware/guest_jwt.go
@@ -0,0 +1,24 @@
+package middleware
+
+import (
+	"go-devops-admin/pkg/jwt"
+	"go-devops-admin/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GuestJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) > 0 {
+			// 解析 token
+			_, err := jwt.NewJWT().ParserToken(c)
+			if err != nil {
+				response.Unauthorized(c, "请使用游客身份访问")
+				c.Abort()
+				return
+			}
+		}
+
+		c.Next()
+	}
+}
