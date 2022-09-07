3c321be42aa99c00617a55563eefa31f3eeb7fe4
蒋宏飞 <jianghongfei@cnstrong.cn>
2022-09-07 18:06:20 +0800

    参数格式化(str包)

---
+++ pkg/str/str.go
@@ -0,0 +1,33 @@
+package str
+
+// go-pluralize 用来处理英文单复数
+// strcase 是用来处理大小写
+import (
+	"github.com/gertd/go-pluralize"
+	"github.com/iancoleman/strcase"
+)
+
+// 转为复数  user -> users
+func Plural(word string) string {
+	return pluralize.NewClient().Plural(word)
+}
+
+// 转为单数 users -> user
+func Singular(word string) string {
+	return pluralize.NewClient().Singular(word)
+}
+
+// 转为 snake_case 蛇形命名法, TopicComment -> topic_comment
+func Snake(word string) string {
+	return strcase.ToSnake(word)
+}
+
+// 转为 CamelCase 驼峰形式, topic_comment -> TopicComment
+func Camel(word string) string {
+	return strcase.ToCamel(word)
+}
+
+// 转为全部小写
+func LowCamel(s string) string {
+	return strcase.ToLowerCamel(s)
+}
