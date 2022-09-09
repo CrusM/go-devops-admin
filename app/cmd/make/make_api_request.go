52b2cc00009bfa6ee93fd18c5ac473c7493e3e6a
蒋宏飞 <jianghongfei@cnstrong.cn>
2022-09-09 18:05:16 +0800

    make request, 从模板中生成 request 文件

---
+++ app/cmd/make/make_api_request.go
@@ -0,0 +1,28 @@
+package make
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var CmdMakeApiRequest = &cobra.Command{
+	Use:   "request",
+	Short: "Create api request, example: make request user",
+	Run:   runMakeApiRequest,
+	Args:  cobra.ExactArgs(1),
+}
+
+func runMakeApiRequest(cmd *cobra.Command, args []string) {
+
+	model := makeModelFormString(args[0])
+
+	// 组建目录
+	dir := fmt.Sprintf("app/requests/%s/", model.TableName)
+	os.MkdirAll(dir, os.ModePerm)
+
+	// 基于模板创建文件
+	createFileFromStub(dir+model.PackageName+"_request.go", "request/request", model)
+
+}
